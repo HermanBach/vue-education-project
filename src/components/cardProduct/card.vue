6f6eb131beb451c58da1f7de111b4e5b2c5b3642
HermanBach <herman.gashkov@gmail.com>
2023-07-26 22:14:16 +0500

    Добавил контракт

---
+++ src/components/cardProduct/card.vue
@@ -0,0 +1,26 @@
+<template>
+  
+</template>
+
+<script setup>
+const cardProps = defineProps({
+    id: Number,
+    title: String,
+    price: Number,
+    description: String,
+    category: String,
+    image: String,
+    rating: Object
+})
+
+</script>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
